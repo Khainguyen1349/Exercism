0e97bee3c4fb5a37151a6dc9274e60acd24afc0b
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-03 22:54:41 +0100

    Update pangram.py
    Using set, really powerful

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,10 +1,2 @@
 def is_pangram(sentence):
-    letter_count = [l.lower() for l in sentence if l.isalpha()]
-    letter_short = []
-    i = 0
-    for l in letter_count:
-        if i == letter_count.index(l):
-            letter_short.append(l)
-        i = i+1
-
-    return len(letter_short) == 26
+    return set(sentence.lower()).issuperset(set("abcdefghijklmnopqrstuvwxyz"))
