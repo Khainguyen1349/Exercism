42b88cd9cc8c622d0e45475cd2b0fed52ca3dc30
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-05 23:27:26 +0100

    Update isbn_verifier.py

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,8 @@
 def is_valid(isbn):
-    pass
+    s = isbn.replace('-', '')
+    if len(s) == 10:
+        if s[-1].isdigit():
+            # transform the whole number and check mod 11
+
+        else:
+            # transform first 9 number and add 10
