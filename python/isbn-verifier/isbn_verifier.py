34078cf56e02d0da108c4d101ae1c23f26dbf80d
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-07 00:58:25 +0100

    Update isbn_verifier.py

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,8 +1,18 @@
 def is_valid(isbn):
     s = isbn.replace('-', '')
-    if len(s) == 10:
-        if s[-1].isdigit():
-            # transform the whole number and check mod 11
+    t = 1
+    if (len(s) == 10) and (s[:-1].isdigit()):
+        t = 0
+        index = 10
+        for i in s[:-1]:
+            t = t + index*int(i)
+            index = index-1
 
+        if s[-1].isdigit():
+            t = t + int(s[-1])
+        elif s[-1] == 'X':
+            t = t + 10
         else:
-            # transform first 9 number and add 10
+            t = 1
+
+    return t % 11 == 0
