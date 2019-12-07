636ee2d6667b76dcfa5888d754e54dcfd0494855
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-07 13:24:13 +0100

    Update hamming.py
    Try to raise Exception

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,7 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise Exception('ValueError')
+    elif len(strand_a) != 0:
+        return sum(1 for counter, value in enumerate(strand_a) if value != strand_b[counter])
+    else:
+        return 0
