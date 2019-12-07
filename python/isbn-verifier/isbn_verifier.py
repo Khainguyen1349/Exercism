09ca8c2e4f410ae4ff50e811999ac540c30d0599
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-07 01:12:46 +0100

    Update isbn_verifier.py

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,18 +1,14 @@
 def is_valid(isbn):
     s = isbn.replace('-', '')
-    t = 1
-    if (len(s) == 10) and (s[:-1].isdigit()):
-        t = 0
+    if (len(s) == 10) and (s[:-1].isdigit()) and (s[-1].isdigit() or s[-1] == 'X'):
+        if s[-1].isdigit():
+            t = int(s[-1])
+        else:
+            t = 10
         index = 10
         for i in s[:-1]:
             t = t + index*int(i)
             index = index-1
-
-        if s[-1].isdigit():
-            t = t + int(s[-1])
-        elif s[-1] == 'X':
-            t = t + 10
-        else:
-            t = 1
-
-    return t % 11 == 0
+        return t % 11 == 0
+    else:
+        return False
