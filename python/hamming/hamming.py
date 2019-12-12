d6f5f65fd94e150c21615e14b165c7d34f219c81
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-12 23:25:15 +0100

    Update hamming.py

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,6 +1,6 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
-        raise Exception('ValueError')
+        raise ValueError('Lengths of texts are not equal')
     elif len(strand_a) != 0:
         return sum(1 for counter, value in enumerate(strand_a) if value != strand_b[counter])
     else:
