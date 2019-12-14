96c9f6c2db9c0d66c2acf97534b492ba27db6f41
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-14 22:21:38 +0100

    Update armstrong_numbers.py

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -5,11 +5,12 @@ def is_armstrong_number(number):
     if(number == 0):
         return True
     else:
+        number_digit = math.floor(math.log10(number)) + 1
         number_temp = number
         i = 1
         digit = []
-        while(i < math.floor(math.log10(number)) + 2):
+        while(i < number_digit + 1):
             digit.append(number_temp % 10)
             number_temp = math.floor(number_temp/10)
             i = i+1
-        return number == sum(math.pow(d, math.floor(math.log10(number)) + 1) for d in digit)
+        return number == sum(math.pow(d, number_digit) for d in digit)
