dfc9dd84f6e99353cc2346f39bff018ae62cd2d2
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-24 00:40:03 +0100

    Giving first solution to bob excercise

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,12 @@
 def response(hey_bob):
-    pass
+    hey_bob_mod = ''.join(hey_bob.split())
+    if len(hey_bob_mod) == 0:
+        return 'Fine. Be that way!'
+    elif hey_bob_mod[-1] == '?' and hey_bob_mod == hey_bob_mod.upper() and len([c for c in hey_bob_mod if c.isalpha()]) != 0:
+        return "Calm down, I know what I'm doing!"
+    elif hey_bob_mod[-1] == '?':
+        return 'Sure.'
+    elif hey_bob_mod == hey_bob_mod.upper() and len([c for c in hey_bob_mod if c.isalpha()]) != 0:
+        return 'Whoa, chill out!'
+    else:
+        return 'Whatever.'
