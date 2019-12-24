47a9d02043f50709efbce3d5eb332b1a1613c6ea
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-24 01:27:17 +0100

    Optimizing hey_bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,14 +1,12 @@
 def response(hey_bob):
-    hey_bob_mod = ''.join(hey_bob.split())
-    hey_bob_isyelling = hey_bob_mod == hey_bob_mod.upper() and len(
-        [c for c in hey_bob_mod if c.isalpha()]) != 0
-    if len(hey_bob_mod) == 0:
+    hey_bob_mod = hey_bob.strip()
+    is_yelling = hey_bob_mod == hey_bob_mod.upper() and any([c.isalpha() for c in hey_bob_mod])
+    if hey_bob_mod == "":
         return 'Fine. Be that way!'
-    elif hey_bob_mod[-1] == '?' and hey_bob_isyelling:
+    if hey_bob_mod[-1] == '?' and is_yelling:
         return "Calm down, I know what I'm doing!"
-    elif hey_bob_mod[-1] == '?':
+    if hey_bob_mod[-1] == '?':
         return 'Sure.'
-    elif hey_bob_isyelling:
+    if is_yelling:
         return 'Whoa, chill out!'
-    else:
-        return 'Whatever.'
+    return 'Whatever.'
