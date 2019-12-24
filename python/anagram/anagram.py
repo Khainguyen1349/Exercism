abc23a8dc881da21bb7e39c83d5585d3e2637eb9
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-24 01:02:28 +0100

    Giving solution for find_anagrams

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,3 @@
 def find_anagrams(word, candidates):
-    pass
+    word_sorted = sorted(word.lower())
+    return [e for e in candidates if sorted(e.lower()) == word_sorted and e.lower() != word.lower()]
