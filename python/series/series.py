a94492ee2e847a7088bfbee531de7adc1b1ad045
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-26 00:49:10 +0100

    Solving series excercise

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,6 @@
 def slices(series, length):
-    pass
+    if length < 1:
+        raise ValueError("Asking for zero or negative length number!")
+    if length > len(series):
+        raise ValueError("Input has its length shorter than asked length!")
+    return [series[counter:counter + length] for counter in range(0, len(series)-length+1)]
