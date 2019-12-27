387d67202b94041a3898835636be96f643c70d60
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-28 00:10:20 +0100

    Solved prime-factor exercise

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,9 +1,12 @@
 def factors(value):
     if value == 1:
         return []
-    f = 1
-    for i in range(2, value):
+    not_found_factor = True
+    for i in range(2, int(value**0.5)+1):
         if value % i == 0:
             f = i
+            not_found_factor = False
             break
-    return factors(int(value/f)).append(f)
+    if not_found_factor:
+        return [value]
+    return [f] + factors(int(value/f))
