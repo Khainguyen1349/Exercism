32dd1fd8dbe293d710664ac6abb897de053c33d4
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2019-12-28 01:06:32 +0100

    Try prime with for loop

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,12 +1,10 @@
 def factors(value):
-    if value == 1:
-        return []
-    not_found_factor = True
-    for i in range(2, int(value**0.5)+1):
-        if value % i == 0:
-            f = i
-            not_found_factor = False
-            break
-    if not_found_factor:
-        return [value]
-    return [f] + factors(int(value/f))
+    # if value == 2:
+    #    return [2]
+    value_temp = value
+    f = []
+    for i in range(2, int(value)):
+        while (value_temp % i) == 0:
+            value_temp = int(value_temp/i)
+            f.append(i)
+    return f
