10725498344443102ff203acdeec862d4b7f9e16
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2020-01-01 23:57:45 +0100

    update twelve-days exercise

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,18 +1,16 @@
 def recite(start_verse, end_verse):
-    things = ["and a Partridge in a Pear Tree.", "two Turtle Doves, ",
-              "three French Hens, ", "four Calling Birds, ", "five Gold Rings, ",
-              "six Geese-a-Laying, ", "seven Swans-a-Swimming, ", "eight Maids-a-Milking, ",
-              "nine Ladies Dancing, ", "ten Lords-a-Leaping, ", "eleven Pipers Piping, ",
-              "twelve Drummers Drumming, "]
+    gifts = ["and a Partridge in a Pear Tree.", "two Turtle Doves, ",
+             "three French Hens, ", "four Calling Birds, ", "five Gold Rings, ",
+             "six Geese-a-Laying, ", "seven Swans-a-Swimming, ", "eight Maids-a-Milking, ",
+             "nine Ladies Dancing, ", "ten Lords-a-Leaping, ", "eleven Pipers Piping, ",
+             "twelve Drummers Drumming, "]
     numbers = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh',
                'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth']
     r = []
-    for i in range(start_verse-1, end_verse):
-        if i != 0:
-            r.append('On the ' + numbers[i] +
-                     ' day of Christmas my true love gave to me: ' +
-                     ''.join([things[i-j] for j in range(i+1)]))
-        else:
-            r.append("On the first day of Christmas my true love gave to me: "
-                     "a Partridge in a Pear Tree.")
+    for verse_number in range(start_verse-1, end_verse):
+        s = 'On the ' + numbers[verse_number] + ' day of Christmas my true love gave to me: ' + \
+            ''.join([gifts[gift_number] for gift_number in range(verse_number, -1, -1)])
+        if verse_number == 0:
+            s = s.replace('and ', '')
+        r.append(s)
     return r
