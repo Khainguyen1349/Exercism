e1eb09a9d46e5bf0ac68888ade5f1c9e5489eb3f
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2020-01-01 23:58:13 +0100

    Solve allergies exercise

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,12 +1,13 @@
 class Allergies:
 
     def __init__(self, score):
-        dict = {}
-        self.allergies = []
+        all = ['eggs', 'peanuts', 'shellfish', 'strawberries',
+               'tomatoes', 'chocolate', 'pollen', 'cats']
+        self.allergies = [all[i] for i in range(8) if score & 2**i]
 
     def allergic_to(self, item):
         return item in self.allergies
 
     @property
     def lst(self):
-        list(self.allergies)
+        return list(self.allergies)
