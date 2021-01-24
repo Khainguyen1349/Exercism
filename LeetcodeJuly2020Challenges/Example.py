d868332d53eab5aeab2146031f54f67deb6974e6
Nguyen Tran Quang Khai <34240337+Khainguyen1349@users.noreply.github.com>
2021-01-24 15:59:35 +0100

    Create Example.py

---
+++ LeetcodeJuly2020Challenges/Example.py
@@ -0,0 +1,14 @@
+import time
+
+start = time.time()
+
+class Solution:
+    def Function(inputs):
+    
+inputs = []
+h = Solution()
+print(h.Function(inputs))
+
+end = time.time()
+print(end - start)
+        
